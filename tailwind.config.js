1088dea41699d57ef9644c9f45072bf830551bd3
Nathan Dinh <nathan.dinh03@outlook.com>
2024-03-06 02:29:02 -0500

    Updated config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/pages/*.{html,js}", "./src/*.{html,js}", "./index.html"],
+  content: ["./src/pages/*.{html,js}", "./src/*.{html,js}", "./index.html","./src/shared/*.{html,js}",],
   darkMode: "selector",
   daisyui: {
     themes: ["dark","light","halloween"],
