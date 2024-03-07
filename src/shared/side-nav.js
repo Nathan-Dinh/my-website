a76095db6b6f4cf19c2ec0cf8afe21716f8f52b1
Nathan Dinh <nathan.dinh03@outlook.com>
2024-03-07 15:24:48 -0500

    Added transition to nav items

--- src/shared/side-nav.js
+++ src/shared/side-nav.js
@@ -91,113 +91,123 @@ export class SideNavigation extends LitElement {
   }
 
   render() {
-    return html`<nav class="relative group flex flex-col">
-      <a
-        @click=${this.handleNavigationClick}
-        class=" mb-4 hover:text-blue-500 flex items-center"
-        href="/"
-      >
-        <svg
-          class="h-8 w-8 text-gray-500"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+    return html`<nav class="flex flex-col">
+      <div class="relative group">
+        <a
+          @click=${this.handleNavigationClick}
+          class=" mb-4  flex items-center"
+          href="/"
         >
-          <path
+          <svg
+            class="h-8 w-8 text-gray-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+            />
+          </svg>
+          <p
+            class="ml-4 invisible group-hover:visible absolute group-hover:relative transform -translate-x-1/4 group-hover:translate-x-0 transition-transform duration-200 ease-in-out"
+          >
+            Home
+          </p>
+        </a>
+      </div>
+
+      <div class="relative group">
+        <a
+          class=" mb-4 flex items-center"
+          @click=${this.handleNavigationClick}
+          href="/projects"
+          ><svg
+            class="h-8 w-8 text-gray-500"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
             stroke-linecap="round"
             stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <polyline points="5 7 10 12 5 17" />
+            <line x1="13" y1="17" x2="19" y2="17" />
+          </svg>
+          <p
+            class="ml-4 invisible group-hover:visible absolute group-hover:relative transform -translate-x-1/4 group-hover:translate-x-0 transition-transform duration-200 ease-in-out"
+          >
+            Projects
+          </p>
+        </a>
+      </div>
+
+      <div class="relative group">
+        <a
+          class=" mb-4 flex items-center"
+          @click=${this.handleNavigationClick}
+          href="/skills"
+        >
+          <svg
+            class="h-8 w-8 text-gray-500"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
             stroke-width="2"
-            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
-          />
-        </svg>
-        <p
-          class="hidden ml-4 group-hover:opacity-100 group-hover:block transition-opacity duration-300"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <polyline points="12 4 4 8 12 12 20 8 12 4" />
+            <polyline points="4 12 12 16 20 12" />
+            <polyline points="4 16 12 20 20 16" />
+          </svg>
+          <p
+            class="ml-4 invisible group-hover:visible absolute group-hover:relative transform -translate-x-1/4 group-hover:translate-x-0 transition-transform duration-200 ease-in-out"
+          >
+            Skills
+          </p></a
         >
-          Home
-        </p>
-      </a>
-      <a
-        class=" mb-4 hover:text-blue-500 flex items-center"
-        @click=${this.handleNavigationClick}
-        href="/projects"
-        ><svg
-          class="h-8 w-8 text-gray-500"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" />
-          <polyline points="5 7 10 12 5 17" />
-          <line x1="13" y1="17" x2="19" y2="17" />
-        </svg>
-        <p
-          class="hidden ml-4 group-hover:opacity-100 group-hover:block transition-opacity duration-300"
-        >
-          Projects
-        </p>
-      </a>
-      <a
-        class=" mb-4 hover:text-blue-500 flex items-center"
-        @click=${this.handleNavigationClick}
-        href="/skills"
-      >
-        <svg
-          class="h-8 w-8 text-gray-500"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" />
-          <polyline points="12 4 4 8 12 12 20 8 12 4" />
-          <polyline points="4 12 12 16 20 12" />
-          <polyline points="4 16 12 20 20 16" />
-        </svg>
-        <p
-          class="hidden ml-4 group-hover:opacity-100 group-hover:block transition-opacity duration-300"
-        >
-          Skills
-        </p></a
-      >
-      <a
-        class=" mb-4 hover:text-blue-500 flex items-center"
-        @click=${this.handleNavigationClick}
-        href="/socials"
-        ><svg
-          class="h-8 w-8 text-gray-500"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          stroke-width="2"
-          stroke="currentColor"
-          fill="none"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        >
-          <path stroke="none" d="M0 0h24v24H0z" />
-          <circle cx="12" cy="5" r="2" />
-          <circle cx="5" cy="19" r="2" />
-          <circle cx="19" cy="19" r="2" />
-          <circle cx="12" cy="14" r="3" />
-          <line x1="12" y1="7" x2="12" y2="11" />
-          <path d="M6.7 17.8l2.8 -2" />
-          <path d="M17.3 17.8l-2.8 -2" />
-        </svg>
-        <p
-          class="hidden ml-4 group-hover:opacity-100 group-hover:block transition-opacity duration-300"
-        >
-          Socials
-        </p>
-      </a>
+      </div>
+      <div class="relative group">
+        <a
+          class=" mb-4  flex items-center"
+          @click=${this.handleNavigationClick}
+          href="/socials"
+          ><svg
+            class="h-8 w-8 text-gray-500"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <path stroke="none" d="M0 0h24v24H0z" />
+            <circle cx="12" cy="5" r="2" />
+            <circle cx="5" cy="19" r="2" />
+            <circle cx="19" cy="19" r="2" />
+            <circle cx="12" cy="14" r="3" />
+            <line x1="12" y1="7" x2="12" y2="11" />
+            <path d="M6.7 17.8l2.8 -2" />
+            <path d="M17.3 17.8l-2.8 -2" />
+          </svg>
+          <p
+            class="ml-4 invisible group-hover:visible absolute group-hover:relative transform -translate-x-1/4 group-hover:translate-x-0 transition-transform duration-200 ease-in-out"
+          >
+            Socials
+          </p>
+        </a>
+      </div>
       <div>
         <img src=${this.src} @click=${this.toggleTheme} alt="icon" id="icon" />
       </div>
