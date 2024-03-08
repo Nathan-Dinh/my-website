f32b471223e4f45ed2d2d58ac412960e9136a85f
Nathan Dinh <nathan.dinh03@outlook.com>
2024-03-08 16:43:55 -0500

    Updated vanta config

--- src/js/vanta/vanta.js
+++ src/js/vanta/vanta.js
@@ -1,14 +1,14 @@
-VANTA.NET({
-    el: "#fanta",
-    mouseControls: true,
-    touchControls: true,
-    gyroControls: false,
-    minHeight: 200.00,
-    minWidth: 200.00,
-    scale: 1.00,
-    scaleMobile: 1.00,
-    color: "#ffffff",
-    points: 9.00,
-    maxDistance: 18.00,
-    backgroundAlpha: 0.00
+VANTA.GLOBE({
+  el: "#fanta",
+  mouseControls: false,
+  touchControls: true ,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  size: 0.30,
+  backgroundAlpha: 0.00,
+  color: "#fb8500",
+  color2: "#ffb703",
   })
