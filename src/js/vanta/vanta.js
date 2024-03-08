cf76e64c13aba51fc705c695e03e2d351d6b8c3e
Nathan Dinh <nathan.dinh03@outlook.com>
2024-03-08 01:01:28 -0500

    Added vanta code

---
+++ src/js/vanta/vanta.js
@@ -0,0 +1,14 @@
+VANTA.NET({
+    el: "#fanta",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: "#ffffff",
+    points: 9.00,
+    maxDistance: 18.00,
+    backgroundAlpha: 0.00
+  })
